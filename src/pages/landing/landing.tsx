8e997c8d45461273d5123b43a53a500a37c77530
kkomyoung <kya754@gmail.com>
2023-10-14 01:23:18 +0900

    feat: landing 100vh 오류 수정

--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import symbol from "../../assets/images/symbol.png"
 import textLogo from "../../assets/images/text_logo.png"
 import styles from "./landing.module.scss"
 
 const Landing = () => {
+    function setScreenSize() {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`); //"--vh"라는 속성으로 정의해준다.
+    }
+
+    useEffect(() => {
+        setScreenSize();
+    }, []);
+
+    window.addEventListener('resize', () => setScreenSize());
+
     return (
         <section className={styles.landingWrap}>
             <div className={styles.halfBg} aria-hidden={true}>
